1b3c1574b13369de0fb3fb3cb7530f2114fcdaa2
Menchynskyi <the.mngs@yahoo.com>
2019-11-25 17:35:25 +0200

    Updates

---
+++ precache-manifest.416799633a7aabe95c1f4c71e8ef1b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c46013a4de282da6ac9be57fce9d27",
+    "url": "/redux-store/index.html"
+  },
+  {
+    "revision": "135c7fb79703a48041de",
+    "url": "/redux-store/static/css/main.3e5499a4.chunk.css"
+  },
+  {
+    "revision": "7ece923a29f162185654",
+    "url": "/redux-store/static/js/2.8a4283b6.chunk.js"
+  },
+  {
+    "revision": "135c7fb79703a48041de",
+    "url": "/redux-store/static/js/main.4da157ce.chunk.js"
+  },
+  {
+    "revision": "6f0f994c6c7721b6c669",
+    "url": "/redux-store/static/js/runtime-main.ce17c7a0.js"
+  },
+  {
+    "revision": "3360c57cdbbebca82e3a792f0a57876c",
+    "url": "/redux-store/static/media/cross.3360c57c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/redux-store/precache-manifest.416799633a7aabe95c1f4c71e8ef1b87.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-store/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
